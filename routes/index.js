a59b8ffefff76f4ca439afad00565114547fa20f
sonukuldeep <sonukuldeep@outlook.com>
2022-11-11 13:46:38 +0530

    return api key

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,12 @@ let cache = apicache.middleware
 
 router.get('/', cache('2 minutes'), async (req, res) => {
     try {
-        const apiRes = await needle('get', `${API_KEY}`, { headers: {"Accept": "application/json"} })
+        // const apiRes = await needle('get', `${API_KEY}`, { headers: {"Accept": "application/json"} })
         
-        const data = apiRes.body
+        // const data = apiRes.body
     
-        res.status(200).json(data)
-        // res.status(200).json({API_KEY})
+        // res.status(200).json(data)
+        res.status(200).json({API_KEY})
         
     } catch (error) {
         res.status(500).json({error})
