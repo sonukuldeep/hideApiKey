4459416b2a01fe199267f8dc84799bc8ce404385
sonukuldeep <sonukuldeep@outlook.com>
2022-11-14 10:41:26 +0530

    fixing small things

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.set('trust proxy', 1)
 
 //enable cors
 app.use(cors({
-    origin: "https://random-private-chat.netlify.app",
+    origin: "https://random-private-chat.netlify.app/",
     methods: ["GET"],
 }))
 
